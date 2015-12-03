8cde0ef116ddab9036d1a756e958a96fdd6bbd77
Matus Chochlik <chochlik@gmail.com>
2015-12-03 16:15:28 +0100

    Updated the reference

---
+++ doc/quickbook/quickref/reflexpr.hpp
@@ -0,0 +1,14 @@
+/*
+ *  Copyright 2015 Matus Chochlik.
+ */
+
+//[reflexpr_usage
+
+typedef reflexpr(::) __MetaGlobalScope; /*<
+A metaobject reflecting the global scope.
+>*/
+typedef reflexpr(int) __MetaType; /*<
+A metaobject reflecting the int type.
+>*/
+//]
+
