70bc57e6ce773ddd206e19b975d22f4e4c4e957e
Matus Chochlik <chochlik@gmail.com>
2016-01-18 13:40:09 +0100

    Multiple changes
    - Updated the examples in the quickbook reference.
    - Updated P0194R0.

---
+++ doc/quickbook/examples/data_members_3.hpp
@@ -0,0 +1,45 @@
+
+/*
+ *  Copyright 2015 Matus Chochlik.
+ */
+
+//[example_data_members_3
+#include __reflexpr_header
+#include <iostream>
+
+struct A
+{
+	int a;
+};
+
+class B : public A
+{
+private:
+	bool b;
+};
+
+class C : public B
+{
+public:
+	char c;
+};
+
+int main(void)
+{
+	using namespace std;
+
+	typedef __reflexpr(A) meta_A;
+	typedef reflexpr(B) meta_B;
+	typedef reflexpr(C) meta_C;
+
+	cout << __meta::__get_size_v<meta::__get_data_members_t<meta_A>> << endl;
+	cout << meta::get_size_v<meta::get_data_members_t<meta_B>> << endl;
+	cout << meta::get_size_v<meta::get_data_members_t<meta_C>> << endl;
+
+	cout << meta::get_size_v<meta::__get_all_data_members_t<meta_A>> << endl;
+	cout << meta::get_size_v<meta::get_all_data_members_t<meta_B>> << endl;
+	cout << meta::get_size_v<meta::get_all_data_members_t<meta_C>> << endl;
+
+	return 0;
+}
+//]
