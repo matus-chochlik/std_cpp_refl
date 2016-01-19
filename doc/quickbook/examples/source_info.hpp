c16e8c025b795a548c10b8c70662cf4f753ab93d
Matus Chochlik <chochlik@gmail.com>
2016-01-19 15:20:25 +0100

    Multiple changes
    - Updated the P0194R0 proposal.
    - Updated the examples in the quickbook reference.

--- doc/quickbook/examples/source_info.hpp
+++ doc/quickbook/examples/source_info.hpp
@@ -13,16 +13,25 @@ int main(void)
 {
 	using namespace std;
 
-	typedef __reflexpr(foo) meta_foo; /*<
+	typedef __reflexpr(int) meta_int; /*<
+	Reflected built-in type [^int].
+	>*/
+	static_assert(__is_metaobject_v<meta_int>, "");
+
+	cout << "name: '" << __meta::__get_name_v<meta_int> << "'" << endl;
+	cout << "file: '" << meta::__get_source_file_v<meta_int> << "'" << endl;
+	cout << "line: " << meta::__get_source_line_v<meta_int> << endl;
+	cout << "col: "<< meta::__get_source_column_v<meta_int> << endl;
+
+	typedef reflexpr(foo) meta_foo; /*<
 	Reflected [^struct foo].
 	>*/
+	static_assert(is_metaobject_v<meta_foo>, "");
 
-	static_assert(__is_metaobject_v<meta_foo>, "");
-
-	cout << "name: " << __meta::__get_name_v<meta_foo> << endl;
-	cout << "file: " << meta::__get_source_file_v<meta_foo> << endl;
-	cout << "line: " << meta::__get_source_line_v<meta_foo> << endl;
-	cout << "col: "<< meta::__get_source_column_v<meta_foo> << endl;
+	cout << "name: '" << meta::get_name_v<meta_foo> << "'" << endl;
+	cout << "file: '" << meta::get_source_file_v<meta_foo> << "'" << endl;
+	cout << "line: " << meta::get_source_line_v<meta_foo> << endl;
+	cout << "col: "<< meta::get_source_column_v<meta_foo> << endl;
 
 	return 0;
 }
