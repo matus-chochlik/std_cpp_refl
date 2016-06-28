dc1e31a5221fb693f7f4c7a638e84503ba691f65
Matus Chochlik <chochlik@gmail.com>
2016-06-28 07:39:43 +0200

    Multiple changes
    - Updated the metaobject concept definitions.
    - Removed the gcc plugin sources.
    - Added the TODO file.

--- doc/graphviz/entities.mk
+++ doc/graphviz/entities.mk
@@ -2,8 +2,8 @@ sinclude tmp/concepts-$(REVISION).mk
 sinclude tmp/traits-$(REVISION).mk
 sinclude tmp/operations-$(REVISION).mk
 
-./tmp/%-$(REVISION).mk: ./%-mk.xsl ../concepts.xml Makefile | tmp
-	$(XSLTPROC) --stringparam revision $(REVISION) --output $@ $< ../concepts.xml
+./tmp/%-$(REVISION).mk: ./%-mk.xsl ../concepts-$(REVISION).xml Makefile | tmp
+	$(XSLTPROC) --stringparam revision $(REVISION) --output $@ $< ../concepts-$(REVISION).xml
 
 DIAGRAMS-$(REVISION) = overview hierarchy traits operations \
 	$(foreach C, $(CONCEPTS-$(REVISION)), concept-$(C)) \
--- doc/graphviz/revision.mk
+++ doc/graphviz/revision.mk
@@ -6,26 +6,26 @@ svg-diagrams-$(REVISION): $(foreach I, $(DIAGRAMS-$(REVISION)), ./out/$I-$(REVIS
 pdf-diagrams-$(REVISION): $(foreach I, $(DIAGRAMS-$(REVISION)), ./out/$I-$(REVISION).pdf)
 .PHONY: pdf-diagrams-$(REVISION)
 
-./tmp/%-$(REVISION).dot: ./%-dot.xsl ../concepts.xml
+./tmp/%-$(REVISION).dot: ./%-dot.xsl ../concepts-$(REVISION).xml
 	$(XSLTPROC) --stringparam revision $(REVISION) --output $@ $^
 
-./tmp/overview-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/overview-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) --output $@ --input $<
 
-./tmp/hierarchy-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/hierarchy-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) -go 0 -gt 0 --output $@ --input $<
 
-./tmp/operations-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/operations-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) -go 1 -gt 0 --output $@ --input $<
 
-./tmp/traits-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/traits-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) -go 0 -gt 1 --output $@ --input $<
 
-./tmp/concept-%-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/concept-%-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) --metaobject $* --output $@ --input $<
 
-./tmp/operation-%-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/operation-%-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) --operation $* --output $@ --input $<
 
-./tmp/trait-%-$(REVISION).dot: ../concepts.xml diagram-dot.py Makefile | tmp
+./tmp/trait-%-$(REVISION).dot: ../concepts-$(REVISION).xml diagram-dot.py Makefile | tmp
 	./diagram-dot.py $(DIAGDOTFLAGS) --trait $* --output $@ --input $<
